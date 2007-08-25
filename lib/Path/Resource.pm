be1819eb291039f601dc76105dabd56950a0b2d8
Robert Krimen <rkrimen@cpan.org>
2007-08-25 23:14:37 +0000

    Import of RKRIMEN/Path-Resource-0.06 from CPAN.
    gitpan-cpan-distribution: Path-Resource
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         RKRIMEN/Path-Resource-0.06.tar.gz
    gitpan-cpan-author:       RKRIMEN
    gitpan-cpan-maturity:     released

--- lib/Path/Resource.pm
+++ lib/Path/Resource.pm
@@ -9,7 +9,7 @@ Path::Resource - URI/Path::Class combination.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =head1 SYNOPSIS
 
@@ -32,10 +32,23 @@ Version 0.05
   my $size = -s $banana_txt_rsc->file;
 
   redirect($banana_txt_rsc->uri);
+  # Redirect to "http://hostname/loc/apple/banana.txt"
+
+=head1 DESCRIPTION
+
+Path::Resource is a module for combining local file and directory manipulation with URI manipulation. It allows you to
+effortlessly map local file locations to their URI equivalent.
+
+It combines Path::Class and URI into one object.
+
+Given a base Path::Resource, you can descend (using ->child) or ascend (using ->parent) the path tree while maintaining
+URI equivalency, all in one object. 
+
+As a convenience, if you do not need the full URI, you can use the ->loc method to just return the URI path.
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Path::Class();
 use Path::Resource::Base();
@@ -45,9 +58,60 @@ use Carp;
 use base qw/Class::Accessor::Fast/;
 __PACKAGE__->mk_accessors(qw(_path base));
 
+=head1 METHODS 
+
 =over 4
 
-=item Path::Resource->new
+=item $rsc = Path::Resource->new
+
+=item $rsc = Path::Resource->new( dir => $dir, uri => $uri, [ path => $path ] )
+
+Create and return a new Path::Resource object using $dir as the base dir and $uri as the base uri.
+
+The URI path of $uri will be automatically extracted and used as the base loc.
+
+If $path is given, then the $rsc will start at that point on the path.
+
+    # For example, if the following $rsc is created like so:
+    my $rsc = Path::Resource->new(uri => "http://example.com/a", dir => "/home/b/htdocs", path => "xyzzy");
+
+    my $dir = $rsc->dir; # The dir "/home/b/htdocs/xyzzy"
+    my $uri = $rsc->uri; # The uri "http://example.com/a/xyzzy"
+
+    # Note that path doesn't have to be a dir.
+    # You can give it a file path if you like (Path::Resource doesn't care)
+    $rsc = Path::Resource->new(uri => "http://example.com/a", dir => "/home/b/htdocs", path => "xyzzy/nothing.txt");
+
+    my $file = $rsc->file; # The file "/home/b/htdocs/xyzzy/nothing.txt"
+    $uri = $rsc->uri; # The uri "http://example.com/a/xyzzy/nothing.txt"
+
+=item $rsc = Path::Resource->new( dir => $dir, uri => $uri, loc => $loc, [ path => $path ] )
+
+Create and return a new Path::Resource object using $dir as the base dir, $uri as the base uri, and
+using $loc as the base loc (the uri path).
+
+If $loc is relative, then it will be appended to $uri->path, otherwise (being absolute) it will replace $uri->path.
+
+If $path is given, then the $rsc will start at that point on the path.
+
+    # For example, if the following $rsc is created like so:
+    my $rsc = Path::Resource->new(uri => "http://example.com/a", dir => "/home/b/htdocs", loc => "c");
+
+    my $dir = $rsc->dir; # The dir "/home/b/htdocs"
+    my $uri = $rsc->uri; # The uri "http://example.com/a/c"
+
+    # On the other hand:
+    $rsc = Path::Resource->new(uri => "http://example.com/a", dir => "/home/b/htdocs", loc => "/g/h");
+
+    $dir = $rsc->dir; # The dir "/home/b/htdocs"
+    $uri = $rsc->uri; # The uri "http://example.com/g/h
+
+=item $rsc = Path::Resource->new( file => $file, dir => $dir, uri => $uri, [ loc => $loc, path => $path ] )
+
+Create and return a new Path::Resource object using $dir as the base dir, $uri as the base uri, and
+the difference between $file and $dir as the path (literally: $path = $file->relative($dir))
+
+If $loc is given then if it is relative, then it will be appended to $uri->path, otherwise (being absolute) it will replace $uri->path.
 
 =cut
 
@@ -62,12 +126,16 @@ sub new {
 
 	my $base;
 	if ($base = $_{base}) {
+        # Use supplied base object
+        croak "\$base ($base) is not of Path::Resource::Base" unless $base->isa("Path::Resource::Base");
 	}
 	else {
+        # Make a new base object from @_
 		if ($dir && $file && $path) {
 			croak "Can't initialize a dir ($dir), a file ($file), and a path ($path) at the same time"
 		}
 		elsif ($dir && $file) {
+            # We were given a dir and file, so keep the dir and determine the path by finding difference between the two.
 			$dir = Path::Class::dir($dir) unless blessed $dir && $dir->isa("Path::Class::Dir");
 			$file = Path::Class::file($file) unless blessed $file && $file->isa("Path::Class::File");
 			croak "Can't initialize since dir ($dir) does not contain file ($file) unless $dir->subsumes($file)";
@@ -131,6 +199,8 @@ sub clone {
 	return __PACKAGE__->new(base => $self->base->clone, path => $path);
 }
 
+=item $rsc->subdir( <part>, [ <part>, ..., <part> ] )
+
 =item $rsc->child( <part>, [ <part>, ..., <part> ] )
 
 Return a clone Path::Resource object whose path is the child of $rsc->path
@@ -140,6 +210,9 @@ Return a clone Path::Resource object whose path is the child of $rsc->path
     # $rsc->path is "b/c/d.tmp"
     $rsc = $rsc->child("c/d.tmp");
 
+    # ->subdir is an alias for ->child
+    $rsc = $rsc->parent->subdir("e");
+
 =cut
 
 sub child {
@@ -147,6 +220,7 @@ sub child {
 	my $clone = $self->clone($self->_path->child(@_));
 	return $clone;
 }
+*subdir = \&child;
 
 =item $rsc->parent
 
@@ -226,6 +300,8 @@ sub uri {
 
 Return a Path::Class::File object based on $rsc->base->dir, $rsc->path, and any optional <part> passed through
 
+NOTE: This method will return a Path::Class::File object, *NOT* a new Path::Resource object (use ->child for that functionality)
+
     my $rsc = Path::Resource->new(dir => "/a", path => "b");
     $rsc = $rsc->child("c/d.tmp");
 
@@ -276,6 +352,10 @@ Return the Path::Resource::Base object for $rsc
 =head1 AUTHOR
 
 Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 SEE ALSO
+
+URI::ToDisk
 
 =head1 BUGS
 
